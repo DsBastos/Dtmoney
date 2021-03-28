64209a6e506904806ff7a592699162575e9e137a
Daniel Bastos <dsbastos1@hotmail.com>
2021-03-28 17:38:13 +0100

    28/03 17:38

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsContext } from "./TransactionsContext";
 
 import { GlobalStyle } from "./styles/global";
 
@@ -20,7 +21,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
@@ -28,7 +29,7 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,8 +13,6 @@ export function Header({ onOpenNewTransactionModal }: HeaderProps) {
                 <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </button>
-
-
             </Content>
         </Container>
     )
